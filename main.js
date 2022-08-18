7fa9c1770db781e4b5bbe9899b59fc5c59e46e25
Luiz Conde <luizconde.dev@outlook.com>
2022-08-18 16:45:11 -0300

    Adicionando Sistema de Dificuldade

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 /* Variaveis */ 
-const screen1 = document.querySelector(".screen1")
-const screen2 = document.querySelector(".screen2")
-const btnTry = document.querySelector("#btn-try")
+const screenSelect = document.querySelector(".screen-select")
+const screenEasy = document.querySelector(".screen-easy")
+const screenHard = document.querySelector(".screen-hard")
+const screenResult = document.querySelector(".screen-result")
+const btnSelectEasy =document.querySelector(".btn-select-easy")
+const btnSelectHard = document.querySelector(".btn-select-hard")
+const btnTryEasy = document.querySelector(".btn-try-easy")
+const btnTryHard = document.querySelector(".btn-try-hard")
 const btnRefresh = document.querySelector("#btn-refresh")
 let i = 1
 
 /* Callback Functions */ 
 
-const getRandom = () => {
+const getRandomEasy = () => {
     const min = Math.ceil(1)
     const max = Math.floor(10)
     let number = (Math.random() * (max - min) + min).toFixed(0);
@@ -15,41 +20,90 @@ const getRandom = () => {
     return number
 }
 
+const getRandomHard = () => {
+    const min = Math.ceil(1)
+    const max = Math.floor(50)
+    let number = (Math.random() * (max - min) + min).toFixed(0);
+    
+    return number
+}
 
-const randomNumber = getRandom()
+const randomNumberEasy = getRandomEasy()
+const randomNumberHard = getRandomHard()
 
-const handleTryClick = () => {
+const selectEasyClick = () => {
+    toggleScreenEasy()
+}
+
+const selectHardClick = () => {
+    toggleScreenHard()
+}
+
+const handleTryEasy = () => {
     event.preventDefault()
-    const inputNumber = document.querySelector("#inputNumber")
+    const inputNumber = document.querySelector("#inputNumberEasy")
 
-    if(Number(inputNumber.value) == randomNumber){
-        toggleScreen()
-        screen2.querySelector("h2").innerText = `Acertou em ${i} tentativas`
+    if(Number(inputNumber.value) == randomNumberEasy){
+        toggleScreenResultEasy()
+        screenResult.querySelector("h2").innerText = `Acertou em ${i} tentativas`
     }else{
         inputNumber.value = ""
-        document.querySelector("#tryAgain").innerText = "Você errou, tente novamente!"
+        document.querySelector("#tryAgainEasy").innerText = "Você errou, tente novamente!"
+    }
+    i++
+}
+
+const handleTryHard = () => {
+    event.preventDefault()
+    const inputNumber = document.querySelector("#inputNumberHard")
+
+    if(Number(inputNumber.value) == randomNumberHard){
+        toggleScreenResultHard()
+        screenResult.querySelector("h2").innerText = `Acertou em ${i} tentativas`
+    }else{
+        inputNumber.value = ""
+        document.querySelector("#tryAgainHard").innerText = "Você errou, tente novamente!"
     }
     i++
     
+    console.log(randomNumberHard)
 }
 
 const refreshPage = () => {
     location.reload()
 }
 
-const toggleScreen = () =>  {
-    screen1.classList.toggle("hide")
-    screen2.classList.toggle("hide")
+const toggleScreenEasy = () =>  {
+    screenSelect.classList.toggle("hide")
+    screenEasy.classList.toggle("hide")
+}
+
+const toggleScreenHard = () =>  {
+    screenSelect.classList.toggle("hide")
+    screenHard.classList.toggle("hide")
+}
+
+const toggleScreenResultEasy = () => {
+    screenEasy.classList.toggle("hide")
+    screenResult.classList.toggle("hide")
+}
+
+const toggleScreenResultHard = () => {
+    screenHard.classList.toggle("hide")
+    screenResult.classList.toggle("hide")
 }
 
 /* Events */ 
 
 /* Events */ 
 
-btnTry.addEventListener("click", handleTryClick)
+btnSelectEasy.addEventListener("click", selectEasyClick)
+btnSelectHard.addEventListener("click", selectHardClick)
+btnTryEasy.addEventListener("click", handleTryEasy)
+btnTryHard.addEventListener("click", handleTryHard)
 btnRefresh.addEventListener("click", refreshPage)
 document.addEventListener("keydown", function(e){
-    if(e.key == "Enter" && screen1.classList.contains("hide")){
+    if(e.key == "Enter" && screenEasy.classList.contains("hide") && screenSelect.classList.contains("hide") && screenHard.classList.contains("hide")){
         refreshPage()
     }
 })
